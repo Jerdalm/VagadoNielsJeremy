3785fab3cf2bdbd839f544d9e8f3790cc6408e18
Jerdalm <36266491+Jerdalm@users.noreply.github.com>
2020-04-02 12:49:08 +0200

    custompair

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,26 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<CustomPair> vragenLijst = new ArrayList<CustomPair>();
-        
+        vragenLijst.add();
+
 
         Scanner sc = new Scanner(System.in);
         String antwoord = sc.nextLine();
 
     }
+
+    public class CustomPair {
+        long key;
+        String vraag;
+        ArrayList<String> antwoordOpties = new ArrayList<String>();
+        char correctAntwoord;
+
+        public CustomPair(long aKey, String aVraag, anCorrectAntwoord) {
+            key = aKey;
+            vraag = aVraag;
+            correctAntwoord = anAntwoord;
+        }
+    }
 }
 
-public class CustomPair {
-    private long key;
-    private String vraag;
-    private char antwoord;
-}
 
