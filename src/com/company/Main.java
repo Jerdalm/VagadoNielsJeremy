a1efc32e006a68172a3c12a11074dc18a7df0cf0
Jerdalm <36266491+Jerdalm@users.noreply.github.com>
2020-04-02 12:28:18 +0200

    base

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,24 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ArrayList<CustomPair> vragenLijst = new ArrayList<CustomPair>();
+        
 
         Scanner sc = new Scanner(System.in);
         String antwoord = sc.nextLine();
 
     }
 }
+
+public class CustomPair {
+    private long key;
+    private String vraag;
+    private char antwoord;
+}
+
